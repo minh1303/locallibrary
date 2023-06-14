c0a5dd30271a406a80280bcdf48b081d987db4d5
Minh <quangminh13032003@gmail.com>
2023-06-14 11:06:46 +0700

    database update

--- db.js
+++ db.js
@@ -1,17 +1,6 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  "locallibrary",
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-    define: {
-      freezeTableName: true,
-    },
-  }
-);
+const sequelize = new Sequelize(process.env.DB_LINK) 
 
 module.exports = sequelize;
